4e2c6422710466d90fef9faa29d9c373136e95c5
Chris Forrest <redrider_cr@yahoo.com>
2020-08-05 19:17:23 -0400

    initial push installed dependencies express,shortid,nodemon,bodyparser made the gitignore and package.json

---
+++ server.js
@@ -0,0 +1,7 @@
+//import dependencies
+const express = require("express");
+const shortid = require("short-id");
+const bodyparser = require("body-parser");
+
+//create a new express server
+const server = express()
