5e90e5e29a897a2a6784c4422fbcbd08e87bc5f2
Chris Forrest <redrider_cr@yahoo.com>
2020-08-05 19:34:44 -0400

    did the require for the dependencies in the server.js made user data and where to listen

--- server.js
+++ server.js
@@ -5,3 +5,30 @@ const bodyparser = require("body-parser");
 
 //create a new express server
 const server = express()
+
+//
+server.use(bodyparser.json());
+
+//created users
+let users = [{
+    id: shortid.generate(),
+    name: "John",
+    bio: "Lives in North Caronlina"
+},{
+    id: shortid.generate(),
+    name: "Jane",
+    bio: "Lastname Doe"
+},{
+    id: shortid.generate(),
+    name: "Xander",
+    bio:"Not enough people named Xander"
+},{
+    id: shortid.generate(),
+    name:"Gwendlyn",
+    bio:"I don't even know if I spelled her name correctly"
+}]
+
+//tell the server where to listen 
+server.listen(8000, () => {
+    console.log("Server started on port 8000")
+})
