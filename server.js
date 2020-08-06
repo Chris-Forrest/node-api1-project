db81f80fadecf30fbca5e787315beebba23a21cc
Chris Forrest <redrider_cr@yahoo.com>
2020-08-05 20:19:27 -0400

    get api/users is working and initial get to / is working get by id not working

--- server.js
+++ server.js
@@ -26,7 +26,49 @@ let users = [{
     id: shortid.generate(),
     name:"Gwendlyn",
     bio:"I don't even know if I spelled her name correctly"
-}]
+}];
+
+//initial server function 
+server.get("/", (req, res) => {
+    res.json({message: 'Hello from my first node project'})
+});
+
+//post request
+server.post("/api/users", (req, res) => {
+    const { name, bio } = req.body;
+    
+    if(!name || !bio ) {
+        return res.status(400).json({ message: 'Please provide name and bio for the user'});
+    }
+    if(users.find(user => user.name === name)) {
+        return res.status(500).json({ message: 'There was an error while saving the user to the database'})
+    }
+
+    const user = { id: shortid.generate(), name, bio };
+    users.push(user);
+    res.status(201).send(user);
+});
+
+//get request
+server.get("/api/users", (req, res) => {
+    if (!req) res.status(500).json({ message: 'The users information could not be retrieved.'});
+    res.send(users);
+});
+
+//get by id
+server.get("/api/users/:id", (req, res) => {
+    const { id } = req.params;
+    const user = users.find(user => user.id == id);
+
+    if(!id) {
+        return res.status(500).json({ message: 'The user information could not be retrieved.'})
+    }
+    if(!user){
+        return res.status(404).json({ mesage: 'The user with the specified ID doesn not exist.'})
+    }
+
+    res.status(200).send(user);
+});
 
 //tell the server where to listen 
 server.listen(8000, () => {
